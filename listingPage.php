2b4c0c33cf6c810ed6cef33687909c28b7cd43a5
Mohsen <63107432+Mohsenrobin@users.noreply.github.com>
2022-09-27 18:48:30 +0100

    Add files via upload

--- listingPage.php
+++ listingPage.php
@@ -1,70 +1,29 @@
 <?php
-//Include functions.php to have access to functions like connection, header and footer
+
 include 'functions.php';
-
-//checking sessions status
-if (session_status() === PHP_SESSION_NONE) {
-  session_start();
-}
-// storing connection to the conn variable 
 $conn = getConnection();
+$results = $results3 = array();
+$bookingRes = array();
+$destination =  $numberOfPeople = $trip_start =  $trip_end = null;
+$bookingRes = array();
 
-//storing temparary results form the first query to this varibale
-$bookingTempResults = array();
-//storing the result from the second query to this variubale
-$bookingResults = array();
-
-
-//checking for array key exist for all the fields 
-if (array_key_exists('destination', $_REQUEST)) {
-  $destinationInput = $_REQUEST['destination'];
-} else {
-  errorHandling("Array key dose not exist");
-}
-
-$destination = mysqli_real_escape_string($conn, $destinationInput);
-
-if (array_key_exists('numberOfPeople', $_REQUEST)) {
-  $numberOfPeopleInput = $_REQUEST['numberOfPeople'];
-} else {
-  errorHandling("Array key dose not exist");
-}
-
-$numberOfPeople = $numberOfPeopleInput;
-
-if (array_key_exists('tripStart', $_REQUEST)) {
-  $tripStartInput = $_REQUEST['tripStart'];
-} else {
-  errorHandling("Array key dose not exist");
-}
-
-$tripStart = $tripStartInput;
-
-if (array_key_exists('tripEnd', $_REQUEST)) {
-  $tripEndInput = $_REQUEST['tripEnd'];
-} else {
-  errorHandling("Array key dose not exist");
-}
-
-$tripEnd =  $tripEndInput;
-
-//checking for empty varibales
+//if statement one by one??????????????????????????????????????????????????????????????
 if (
-  !empty($destination) and !empty($numberOfPeople) and !empty($tripStart)
-  and !empty($tripEnd)
+  !empty($_REQUEST['destination']) and !empty($_REQUEST['numberOfPeople']) and !empty($_REQUEST['tripStart'])
+  and !empty($_REQUEST['tripEnd'])
 ) {
 
-  // First sql query which search for any accommodation that is booked 
-  // and have conflict with the start and end date of user trip to be excluded from the result
+  $destination = mysqli_real_escape_string($conn, $_REQUEST['destination']);
+  $numberOfPeople = mysqli_real_escape_string($conn, $_REQUEST['numberOfPeople']);
+  $trip_start = mysqli_real_escape_string($conn, $_REQUEST['tripStart']);
+  $trip_end = mysqli_real_escape_string($conn, $_REQUEST['tripEnd']);
+
   $sql = "SELECT a.accommodationID,accommodation_name,`description`,price_per_night,`location`,country,shortDesc FROM accommodation a, booking b
       WHERE (`location` = ? or  country = ? ) and (( `start_date` <= ? and `end_date` > ? ) or 
       ( `start_date` < ? and `end_date` >= ? ) or ( `start_date` >= ? and `end_date` < ? )) and a.accommodationID = b.accommodationID ORDER BY `start_date`";
-
-  // using prepare method
   if ($stmt = mysqli_prepare($conn, $sql)) {
 
-    //passing parameters
-    mysqli_stmt_bind_param($stmt, "ssssssss", $destination, $destination, $tripStart, $tripStart, $tripEnd, $tripEnd, $tripStart, $tripEnd);
+    mysqli_stmt_bind_param($stmt, "ssssssss", $destination, $destination, $trip_start, $trip_start, $trip_end, $trip_end, $trip_start, $trip_end);
 
     mysqli_stmt_execute($stmt);
 
@@ -72,34 +31,30 @@ if (
 
     $results = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    //storing ids of alrady booked accommodations
     foreach ($results as $res) {
-      $bookingTempResults[] = $res['accommodationID'];
+      $bookingRes[] = $res['accommodationID'];
     }
+    $bookingUnique = array_unique($bookingRes, SORT_REGULAR);
+    //print_r($bookingUnique);
+    //SELECT ONLY ACCOMMOADTION STAFF TO MAKE IT UNIQE
+    // $bookingUnique = array_unique($results,SORT_REGULAR);
+    // print_r($bookingUnique);
 
-    // deleting duplicated ids
-    $bookingUnique = array_unique($bookingTempResults, SORT_REGULAR);
-
-    //A new table created in the databse called images in order to store images name and url
-    //Second sql query which is for getting all the accommodations and their images
-    $sql2 = "SELECT a.accommodationID,accommodation_name,`description`,price_per_night,`location`,country,accommodationType, shortDesc , imageID, imageName FROM accommodation a, images i
+    $sql = "SELECT a.accommodationID,accommodation_name,`description`,price_per_night,`location`,country,accommodationType, shortDesc , imageID, imageName FROM accommodation a, images i
       WHERE (`location` = ? or  country = ? ) and a.accommodationID = i.accommodationID";
-    //Prepare statement
-    if ($stmt2 = mysqli_prepare($conn, $sql2)) {
+    if ($stmt = mysqli_prepare($conn, $sql)) {
 
-      //passing parameters
-      mysqli_stmt_bind_param($stmt2, "ss", $destination, $destination);
+      mysqli_stmt_bind_param($stmt, "ss", $destination, $destination);
 
-      mysqli_stmt_execute($stmt2);
+      mysqli_stmt_execute($stmt);
 
-      $result2 = mysqli_stmt_get_result($stmt2);
+      $result = mysqli_stmt_get_result($stmt);
 
-      $results2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+      $results = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
-      //Here we delete all the accommodations that are already booked at the specefic date
-      // and store the rest of them to the $bookingResults varible to be printed out on the page
-      foreach ($results2 as $res) {
+      foreach ($results as $res) {
+        // echo "dfsdfsdfsfsfsdf";
         for ($i = 0; $i < count($bookingUnique); $i++) {
           if (isset($res)) {
             if ($res['accommodationID'] == $bookingUnique[$i]) {
@@ -108,24 +63,17 @@ if (
           }
         }
         if (isset($res)) {
-          $bookingResults[] = $res;
+          $bookingRes[] = $res;
         }
       }
-      //Storing the number of people, start date and end date into session global varible to 
-      // be passed to details and profile page
-      $_SESSION['numberOfPeople'] = mysqli_real_escape_string($conn, $numberOfPeople);
-      $_SESSION['start_date'] = mysqli_real_escape_string($conn, $tripStart);
-      $_SESSION['end_date'] = mysqli_real_escape_string($conn, $tripEnd);
     }
-  } else {
-    errorHandling("Seomthing went wrong, data is not loaded from the database");
   }
-  //Free resources
   mysqli_free_result($result);
-  mysqli_free_result($result2);
   mysqli_close($conn);
-} else {
-  errorHandling("At least one of the variables is empty");
+}
+
+if (isset($_GET['filterSubmit'])) {
+  echo " havent set it yet";
 }
 
 ?>
@@ -133,30 +81,45 @@ if (
 <!DOCTYPE html>
 <html lang="en">
 
-<!-- Getting the Header  -->
 <?php echo getHeader('Listing') ?>
 
-<!-- listing content wraps all the contents of the page -->
 <div class="listingContent">
-  <!-- this grid will tyake carte of all the elements -->
   <div id=listingGridContainer>
 
-    <!-- calling main search method  -->
     <?php mainSearchBar() ?>
 
-    <!-- this div lists all the results of the second query -->
+    <div id="nav">
+      <form class="searchNav" action="listingPage.php" method="GET">
+        <a class="filters"><label for="budget"> Fliters </label><br>
+          <hr><br>
+        </a>
+        <a class="budget"><label for="budget"> Your budget <br> (per night) </label><br><br></a>
+        <input type="radio" id="budget1" name="budget" value="0 to £50">
+        <label for="budget1"> 0 - £50 </label><br><br>
+        <input type="radio" id="budget2" name="budget" value="£50 - £100">
+        <label for="budget2"> £50 - £100 </label><br><br>
+        <input type="radio" id="budget3" name="budget" value="£100 - £200">
+        <label for="budget3"> £100 - £200 </label><br><br>
+        <hr>
+        <a class="budget"><label for="Property type">Property type</label><br><br></a>
+        <input type="radio" id="Property type1" name="Property type" value="Hotel">
+        <label for="Property type1"> Hotel </label><br><br>
+        <input type="radio" id="Property type2" name="Property type" value="Guest houses">
+        <label for="Property type2"> Guest houses </label><br><br>
+        <button type="submit" name="filterSubmit">Apply Filter</button>
+      </form>
+
+    </div>
+
     <div class="listing">
-      <?php
-      if (empty($bookingResults)) {
-        errorHandling("There is no any property for this period of time, please change the date or the destinatoin");
-      }
-      foreach ($bookingResults as $res) {
-      ?>
-        <a class="searchResult" href="details.php?id=<?php echo $res['accommodationID'] ?>">
-          <img src="assets/images/<?php echo $res['imageName']; ?>" border="0" alt="" width="350" height="247">
+      <?php foreach ($bookingRes as $res) { ?>
+        <a class="searchResult" href="details.php?id=<?php echo $res['accommodationID'] ?>
+              &people=<?php echo $numberOfPeople ?>
+              &std=<?php echo $trip_start ?>&edd=<?php echo $trip_end ?>
+              &pricePerNight=<?php echo $res['price_per_night'] ?>">
+          <img src="<?php echo $res['imageName']; ?>" border="0" alt="" width="350" height="247">
           <div class="description">
             <p class="listingName"><?php echo $res['accommodation_name']; ?></p>
-            <p class="listingName"><?php echo $res['accommodationID']; ?></p>
             <p class="location"><?php echo $res['location']; ?><br><br><br></p>
             <p class="listingDetails1"><?php echo $res['accommodationType']; ?></p>
             <p class="listingDetails2"><?php echo $res['shortDesc']; ?></p>
@@ -172,7 +135,6 @@ if (
   </div>
 </div>
 
-<!-- Getting the Footer  -->
 <?php echo getFooter() ?>
 
 
--- login.php
+++ login.php
@@ -1,75 +1,35 @@
 <?php
-//Include functions.php to have access to functions like connection, header and footer
-include 'functions.php';
-
-//checking sessions status
 if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
-//getting the connection
+include 'functions.php';
 $conn = getConnection();
-
-//This varibale is used to print out the error.
-$error = null;
-// Check if submit button is pressed by user, so we can evaluate the fields.
+$username;
 if (isset($_POST['submit'])) {
+  $user = mysqli_real_escape_string($conn, $_POST['email']);
+  $sql = "SELECT * FROM customers WHERE username = ?";
 
-  //Checking for array key exist
-  if (array_key_exists('email', $_REQUEST)) {
-    $emailInput = $_REQUEST['email'];
-  } else {
-    errorHandling("Array key dose not exist");
+  if ($stmt = mysqli_prepare($conn, $sql)) {
+
+    mysqli_stmt_bind_param($stmt, "s", $user);
+
+    mysqli_stmt_execute($stmt);
+
+    $queryResult = mysqli_stmt_get_result($stmt);
   }
-
-
-  $username = mysqli_real_escape_string($conn, $emailInput);
-
-  if (array_key_exists('Password', $_REQUEST)) {
-    $passwordInput = $_REQUEST['Password'];
-  } else {
-    errorHandling("Array key dose not exist");
-  }
-
-
-  $password = mysqli_real_escape_string($conn, $passwordInput);
-
-  //Making sure fields are not empty
-  if (!empty($username) or !empty($password)) {
-
-    //looking for the user in the databse.
-    $sql = "SELECT * FROM customers WHERE username = ?";
-
-    if ($stmt = mysqli_prepare($conn, $sql)) {
-
-      mysqli_stmt_bind_param($stmt, "s", $username);
-
-      mysqli_stmt_execute($stmt);
-
-      $result = mysqli_stmt_get_result($stmt);
+  if ($queryResult) {
+    while ($row = mysqli_fetch_assoc($queryResult)) {
+      // $username = mysqli_fetch_assoc($queryResult);
+      $_SESSION["forename"] = $row['customer_forename'];
+      $_SESSION['id'] = $row['customerID'];
+      echo $_SESSION["forename"] . $_SESSION['id'] . " sdfasdf" . $row['customer_forename'];
+      header('location: index.php');
     }
-    if ($result) {
-      //Here we check if user has found then evaluate the password. By using password_verify method we can 
-      // get the password and compare it to the password that user entered.
-      while ($row = mysqli_fetch_assoc($result)) {
-        if (password_verify($password, $row['password_hash'])) {
-          $_SESSION["forename"] = $row['customer_forename'];
-          $_SESSION['id'] = $row['customerID'];
-          echo $_SESSION["forename"] . $_SESSION['id'] . " sdfasdf" . $row['customer_forename'];
-          header('location: index.php');
-        }
-      }
-      //Printing out an error if user or password was incorrect
-      if (empty($error)) {
-        $error = errorHandling("Username or password  is incorrect");
-      }
-    }
-    //Free the result
-    mysqli_free_result($result);
+    // else {
+    echo "user not find";
+    //  }
   }
 }
-
-//Free the result
-mysqli_close($conn);
 
 
 ?>
@@ -77,9 +37,8 @@ mysqli_close($conn);
 
 <!DOCTYPE html>
 <html lang="en">
-<!-- Getting the Header  -->
 <?php echo getHeader('Login') ?>
-<!-- form wrapper -->
+
 <div class="loginBox">
   <form class="loginForm" action="login.php" method="POST">
     <h1>Login</h1>
@@ -88,14 +47,13 @@ mysqli_close($conn);
     <input type="text" placeholder="Email" name="email" id="email" required>
     <br>
     <label for="psw"><b>Password</b></label><br>
-    <input type="password" placeholder="Password" name="Password" id="Password" required>
+    <input type="password" placeholder="Password" name="psw" id="psw" required>
     <br><br>
     <button type="submit" name="submit" class="signInbtn">Login</button>
     <br>
     <p>Don'n have an account ? create one <a href="register.php">Register</a>.</p>
   </form>
 </div>
-<!-- Getting the Footer  -->
 <?php echo getFooter() ?>
 
 
